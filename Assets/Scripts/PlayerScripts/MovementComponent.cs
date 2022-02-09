ee19ec7729a1ee713303576f35e5fea15eff7137
Morgan Ethier <morganethier@gmail.com>
2022-02-08 20:19:31 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerScripts/MovementComponent.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementComponent : MonoBehaviour
+{
+    [SerializeField]
+    float walkSpeed = 5;
+    [SerializeField]
+    float runSpeed = 10;
+    [SerializeField]
+    float jumpForce = 5;
+
+    //components
+    private PlayerController playerController;
+    Rigidbody rigidbody;
+    Animator playerAnimator;
+    public GameObject followTarget;
+
+    //references
+    Vector2 inputVector = Vector2.zero;
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 lookInput = Vector2.zero;
+
+    public float aimSensitivity = 0.2f;
+
+    //animator hashes
+    public readonly int movementXHash = Animator.StringToHash("MovementX");
+    public readonly int movementYHash = Animator.StringToHash("MovementY");
+    public readonly int isJumpingHash = Animator.StringToHash("IsJumping");
+    public readonly int isRunningHash = Animator.StringToHash("IsRunning");
+    public readonly int isFiringHash = Animator.StringToHash("IsFiring");
+
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+        playerController = GetComponent<PlayerController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ///aiming/Looking
+        //horizontal rotation
+        followTarget.transform.rotation *= Quaternion.AngleAxis(lookInput.x * aimSensitivity, Vector3.up);
+        //vertical rotation
+        followTarget.transform.rotation *= Quaternion.AngleAxis(lookInput.y * aimSensitivity, Vector3.left);
+
+        var angles = followTarget.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = followTarget.transform.localEulerAngles.x;
+
+        if (angle > 180 && angle < 340)
+        {
+            angles.x = 340;
+        }
+        else if (angle < 180 && angle > 40)
+        {
+            angles.x = 40;
+        }
+
+        followTarget.transform.localEulerAngles = angles;
+
+        //rotate the player rotation based on the look transform
+        transform.rotation = Quaternion.Euler(0, followTarget.transform.rotation.eulerAngles.y, 0);
+
+        followTarget.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+
+        ///Movement
+        if (playerController.isJumping) return;
+        if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
+
+        moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
+        float currentSpeed = playerController.isRunning ? runSpeed : walkSpeed;
+
+        Vector3 movementDirection = moveDirection * (currentSpeed * Time.deltaTime);
+
+        transform.position += movementDirection;
+
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+        playerAnimator.SetFloat(movementXHash, inputVector.x);
+        playerAnimator.SetFloat(movementYHash, inputVector.y);
+    }
+    public void OnRun(InputValue value)
+    {
+        playerController.isRunning = value.isPressed;
+        playerAnimator.SetBool(isRunningHash, playerController.isRunning);
+    }
+    public void OnJump(InputValue value)
+    {
+        playerController.isJumping = value.isPressed;
+        rigidbody.AddForce((transform.up + moveDirection) * jumpForce, ForceMode.Impulse);
+        playerAnimator.SetBool(isJumpingHash, playerController.isJumping);
+    }
+
+    public void OnAim(InputValue value)
+    {
+        playerController.isAiming = value.isPressed;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+        //if we aim up, down, adjust animations to have a mask that will let us properly animate aim
+    }
+
+    public void OnFire(InputValue value)
+    {
+        playerController.isFiring = value.isPressed;
+        playerAnimator.SetBool(isFiringHash, playerController.isFiring);
+    }
+
+    public void OnReload(InputValue value)
+    { 
+    
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground") && !playerController.isJumping) return;
+
+        playerController.isJumping = false;
+        playerAnimator.SetBool(isJumpingHash, false);
+
+    }
+}
